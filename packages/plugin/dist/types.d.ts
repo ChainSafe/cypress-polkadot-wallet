51a665981ba9b5f88bbeafb064bdc29bd05f65b0
Thibaut Sardan <github@thib.top>
2024-01-04 15:26:57 +0000

    build

---
+++ packages/plugin/dist/types.d.ts
@@ -0,0 +1,5 @@
+import { InjectedAccount } from '@polkadot/extension-inject/types';
+export interface InjectedAccountWitMnemonic extends InjectedAccount {
+    mnemonic: string;
+    publicKey?: string;
+}
