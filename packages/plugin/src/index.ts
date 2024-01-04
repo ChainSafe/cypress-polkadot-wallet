46fcbf2921503c89e09864037529c9d05cc47b64
Thibaut Sardan <github@thib.top>
2024-01-04 17:39:40 +0000

    minor changes

--- packages/plugin/src/index.ts
+++ packages/plugin/src/index.ts
@@ -8,7 +8,7 @@ declare global {
     interface Chainable {
       /**
        * Initialize the Polkadot wallet. If an authorizedDappName is passed there is no need to authorize the first connection for Dapps using this name.
-       * @param {InjectedAccount[]} accounts - Accounts to load into the wallet.
+       * @param {InjectedAccount[]} accounts - Accounts to load into the wallet
        * @param {string | undefined} authorizedDappName - Dapp name to automatically share accounts with, without needing to authorize
        * @param {string | undefined} walletName - Sets the name of the injected wallet (default 'polkadot-js')
        * @example cy.initWallet([{ address: '7NPoMQbiA6trJKkjB35uk96MeJD4PGWkLQLH7k7hXEkZpiba', name: 'Alice', type: 'sr25519'}], 'My-Dapp', 'My-wallet-extension')
@@ -27,15 +27,15 @@ declare global {
 
       /**
        * Approve a specific authentication request for the Dapp to get access the wallet accounts
-       * @param {number} id - the id of the request to authorize. This id is part of the getAuthRequests object response.
+       * @param {number} id - the id of the request to authorize. This id is part of the getAuthRequests object response
        * @param {string[]} accountAddresses - the account addresses to share with the applications. These addresses must be part of the ones shared in the `initWallet`
        * @example cy.approveAuth(1694443839903, ["7NPoMQbiA6trJKkjB35uk96MeJD4PGWkLQLH7k7hXEkZpiba"])
        */
       approveAuth: (id: number, accountAddresses: string[]) => void
 
       /**
-       * Reject a specific authentication request. The Dapp will receive 0 connected wallet as a result.
-       * @param {number} id - the id of the request to reject. This id is part of the getAuthRequests object response.
+       * Reject a specific authentication request. The Dapp will receive 0 connected wallet as a result
+       * @param {number} id - the id of the request to reject. This id is part of the getAuthRequests object response
        * @param {reason} reason - the reason for the rejection
        * @example cy.rejectAuth(1694443839903, "Cancelled")
        */
